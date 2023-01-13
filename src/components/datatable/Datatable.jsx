5832561911ff63b1149a2c505d5e7937b57058c4
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-01-13 07:12:37 +0300

    product category feature

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -88,18 +88,7 @@ function Datatable(props) {
     },
   ];
 
-  const default_data = [
-    { name: 'Joe James', company: 'Test Corp', city: 'Yonkers', state: 'NY' },
-    { name: 'John Walsh', company: 'Test Corp', city: 'Hartford', state: 'CT' },
-    { name: 'Bob Herm', company: 'Test Corp', city: 'Tampa', state: 'FL' },
-    { name: 'Nick Mwedwa', company: 'Edureca', city: 'Kimbo', state: 'FL' },
-    {
-      name: 'James Houston',
-      company: 'Test Corp',
-      city: 'Dallas',
-      state: 'TX',
-    },
-  ];
+  const default_data = [];
 
   let {
     tb_title,
