1e7ee64c57a870241ff8802f92298fa421c1563c
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-01-20 07:54:49 +0300

    dashboard widgets data

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -5,6 +5,27 @@ import { Link } from 'react-router-dom';
 import './widget.css';
 
 function Widget(props) {
+  let { icon, title='Model', data } = props;
+  let active_records = Object.values(data)[0];
+  let inactive_records = Object.values(data)[1];
+  let active_percent = parseInt((active_records/(active_records+inactive_records))*100);
+
+
+
+  let getRecords = () =>(
+    active_records > 98 ? 
+            <Badge badgeContent={'+'} color="info" className="widget-left-item barge">
+              <Typography variant="h4" component="div" className="text-muted">
+                {active_records}
+              </Typography>
+            </Badge>
+            :
+            <Typography variant="h4" component="div" className="text-muted">
+                {active_records}
+            </Typography>
+  )
+
+
   return (
     <>
       <Grid item xs={12} sm={4} md={4} lg={3} className="widget-container">
@@ -14,24 +35,21 @@ function Widget(props) {
 
           <Grid item xs={8} className='widget-left'>
             <Typography variant="h6" component="div" className="widget-left-item widget-left-text">
-              USERS
+              {title}
             </Typography>
-            <Badge badgeContent={'+'} color="info" className="widget-left-item barge">
-              <Typography variant="h4" component="div" className="text-muted">
-                99
-              </Typography>
-            </Badge>
-            <Link href="#" className="widget-left-item widget-link"> View Users</Link>
+             { getRecords()}
+            
+            <Link href="#" className="widget-left-item widget-link"> View {title}</Link>
           </Grid>
 
           <Grid item xs={4}>
             <div className="widget-right">
                 <div className="widget-icon-top">
                     <KeyboardArrowUp className='icon-top'/>
-                    2%
+                    Active: {active_percent}%
                 </div>
                 <div className="widget-icon-bottom">
-                    <People className='icon-bottom'/>
+                    {icon}
                 </div>
             </div>
           </Grid>
--- src/pages/dashboard/pages/homedash/Homedash.jsx
+++ src/pages/dashboard/pages/homedash/Homedash.jsx
@@ -1,4 +1,5 @@
-import { Launch } from '@mui/icons-material';
+import React, { useState, useEffect } from 'react';
+import { Launch, MonetizationOnOutlined, People } from '@mui/icons-material';
 import {
   Button,
   Card,
@@ -8,16 +9,55 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Appbreadcrumb from '../../../../components/breadcrumb/Appbreadcrumb';
 import Chart from '../../../../components/charts/Chart';
 import Datatable from '../../../../components/datatable/Datatable';
 import Featured from '../../../../components/featured/Featured';
+import useAxiosPrivate from '../../../../components/hooks/useAxiosPrivate';
 import Widget from '../../../../components/widget/Widget';
 import './homedash.scss';
 
 function Homedash() {
+  // states
+  const [salesstats, setSalesstats] = useState({});
+  const [usersstats, setUsersstats] = useState({});
+
+  // hooks
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const SETTINGS_URL = 'api/settings/';
+
+
+  useEffect(() => {
+    let isMounted = true;
+    let controller = new AbortController();
+    const getStats = async () => {
+      try {
+        const res = await axiosPrivate.get(SETTINGS_URL+'stats', {
+          signal: controller.signal,
+        });
+        console.log(res.data.users);
+        isMounted && setSalesstats(res?.data?.sales);
+        isMounted && setUsersstats(res?.data?.users);
+
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+
+
+    getStats();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [axiosPrivate, location, navigate]);
+
+
   const breadcrumbs = [
     <Typography key="2" color="text.primary">
       Dashboard
@@ -36,10 +76,10 @@ function Homedash() {
       <Divider light sx={{ mb: 2 }} className="divider" />
 
       <Grid container className="widgets">
-        <Widget />
-        <Widget />
-        <Widget />
-        <Widget />
+        <Widget title={'Users'} data={usersstats} icon={<People className='icon-bottom'/>} />
+         <Widget title={'Sales'} data={salesstats} icon={<MonetizationOnOutlined className='icon-bottom'/>} />
+        {/*<Widget />
+        <Widget /> */}
       </Grid>
 
       <Grid container>
